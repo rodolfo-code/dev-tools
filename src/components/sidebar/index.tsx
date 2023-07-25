180e47613f5e54da01f03e805ed9e47f88be3db4
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2023-07-25 13:31:25 +0100

    incluindo sidebar e estilizacao global

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -4,16 +4,16 @@ const categories = ["Hooks", "Patterns", "Functions"];
 
 export default function Sidebar(props: any) {
     return (
-        <div>
-            <aside>
-                <ul className="inline-flex items-center gap-12">
-                    {categories.map((category: string) => (
-                        <li key={category}>
-                            <Link href={`/content/${category}`}>{category}</Link>
-                        </li>
-                    ))}
-                </ul>
-            </aside>
-        </div>
+        // <div>
+        <aside className="border-2 border-[orange] w-2/12">
+            <ul className="flex flex-col w-full items-center gap-12">
+                {categories.map((category: string) => (
+                    <li key={category}>
+                        <Link href={`/content/${category}`}>{category}</Link>
+                    </li>
+                ))}
+            </ul>
+        </aside>
+        // </div>
     );
 }
