61c8a1ece801453f817caf2284c358be1c0d6428
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2023-07-29 20:36:33 -0300

    criando componente de lista dinamico

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import { useParams, useRouter } from "next/navigation";
+import MenuItem from "../menuItem";
 
 const categories = ["Hooks", "Patterns", "Functions", "Function", "Config", "Inovations", "AI", "Features", "Development"];
 
@@ -12,18 +13,14 @@ export default function Sidebar(props: any) {
             <ul className="flex flex-col h-screem items-center">
                 {categories.map((category: string) => (
                     <>
-                        <li
-                            key={category}
-                            className="flex items-center w-full h-12 text-lunar-600 hover:bg-indi-go-50 
-                        dark:text-lunar-100 dark:hover:bg-lunar-800 dark:hover:text-indi-go-50 duration-300"
-                        >
+                        <MenuItem>
                             <Link className="flex pl-5 p-2.5 w-full" href={`/content/${categorie}/${category}`}>
                                 <div className="flex items-center">
                                     <img src="/icone.png" width="14px" />
                                 </div>
                                 <div className="p-2.5">{category}</div>
                             </Link>
-                        </li>
+                        </MenuItem>
                         <div className="w-full h-[0.8px] bg-gradient-to-r from-lunar-100 via-transparent  to-transparent dark:bg-gradient-to-r dark:from-lunar-800 dark:via-transparent dark:to-transparent duration-300"></div>
                     </>
                 ))}
